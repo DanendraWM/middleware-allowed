bc3020650551f4bc67f0b1f1bbbf709cf5d8da2c
Suntet <sanity2828@gmail.com>
2023-01-24 14:56:45 +0700

    penambahan modal dan perwanaan text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
   let add1 = formatISO9075(addHours(new Date(), 5), {
     representation: "time",
   });
-
   const tanggalNow = format(new Date(), "dd MMMM yyyy", { locale: id });
   console.log(add1);
   console.log(today);
@@ -27,11 +26,12 @@ function App() {
     console.log(nama, instansi, desc, tujuan, date);
     window.location.href = `https://api.whatsapp.com/send?phone=6285156834452&text======= *Izin Ke Middleware* ======%0a%0a%0aTanggal : ${tanggalNow}%0aNama : ${nama}%0ainstansi : ${instansi}%0aTujuan : ${tujuan}%0aKeterangan : ${desc}%0a%0aTerima Kasih`;
   };
+
   return (
     <div className="min-h-screen bg-slate-300 text-black">
       <div className=" flex flex-row w-full text-center bg-[#f5f2f2] shadow-[0_15px_20px_-20px_rgba(0,0,0,0.5)] rounded-b-lg rounded-t-none">
-        <div className="card-body font-bold font-serif ">WP Team</div>
-        <div className="card-body font-semibold font-sans">
+        <div className="card-body font-semibold font-sans ">WestPlatform Team</div>
+        <div className="card-body font-bold font-sans">
           Form Izin Masuk Middleware
         </div>
       </div>
@@ -41,21 +41,21 @@ function App() {
             <div className="card-body"></div>
           </div>
 
-          <div className="card lg:w-2/3 w-4/5 bg-white shadow-[0_25px_20px_-20px_rgba(0,0,0,0.5)]">
+          <div id="header" className="card lg:w-2/3 w-4/5 bg-white shadow-[0_25px_20px_-20px_rgba(0,0,0,0.5)]">
             <div className="card absolute bottom-90 w-full h-5 rounded-b-none bg-[#6f1d1b] shadow-[0_25px_20px_-20px_rgba(0,0,0,0.5)] rounded-md"></div>
             <div className="card-body">
               <h2 className="card-title">Middleware Entry Request</h2>
               <h3 className="text-md">
                 All Data Will Be Handled With Procedure{" "}
               </h3>
-              <p className="text-sm">
+              <p className="text-sm font-medium text-rose-600">
                 *Isi Form Dengan Nyata Tanpa Di Buat-Buat
               </p>
             </div>
           </div>
           <div className="card lg:w-2/3 w-4/5 bg-white shadow-[0_25px_20px_-20px_rgba(0,0,0,0.5)]">
             <div className="card-body">
-              <h2 className="card-title">*Nama Pengaju</h2>
+              <h2 className="card-title"><span className="text-rose-600">*</span>Nama Pengaju</h2>
               <p className="text-sm">Nama pemohon izin</p>
               <input
                 className="bg-transparent  text-black border-black focus:outline-none border-b-2 lg:w-1/3"
@@ -70,7 +70,7 @@ function App() {
           </div>
           <div className="card lg:w-2/3 w-4/5 bg-white shadow-[0_25px_20px_-20px_rgba(0,0,0,0.5)]">
             <div className="card-body">
-              <h2 className="card-title">*Instansi</h2>
+              <h2 className="card-title"><span className="text-rose-600">*</span>Instansi</h2>
               <input
                 className="bg-transparent  text-black border-black focus:outline-none border-b-2 lg:w-1/3"
                 type="text"
@@ -84,7 +84,7 @@ function App() {
           </div>
           <div className="card lg:w-2/3 w-4/5 bg-white shadow-[0_25px_20px_-20px_rgba(0,0,0,0.5)]">
             <div className="card-body">
-              <h2 className="card-title">*Tujuan</h2>
+              <h2 className="card-title"><span className="text-rose-600">*</span>Tujuan</h2>
               <p className="text-sm">Tujuan ke Middleware</p>
               <select
                 defaultValue={"DEFAULT"}
@@ -109,7 +109,7 @@ function App() {
           </div>
           <div className="card lg:w-2/3 w-4/5 bg-white shadow-[0_25px_20px_-20px_rgba(0,0,0,0.5)]">
             <div className="card-body">
-              <h2 className="card-title">*Keterangan</h2>
+              <h2 className="card-title"><span className="text-rose-600">*</span>Keterangan</h2>
               <p className="text-sm">Keterangan Dari pemohon</p>
               <input
                 className="bg-transparent  text-black border-black focus:outline-none border-b-2 lg:w-2/3"
@@ -140,7 +140,7 @@ function App() {
           </div> */}
           <div className="card lg:w-2/3 w-4/5 bg-white shadow-[0_25px_20px_-20px_rgba(0,0,0,0.5)]">
             <div className="card-body">
-              <h2 className="card-title">*waktu</h2>
+              <h2 className="card-title"><span className="text-rose-600">*</span>waktu</h2>
               <p className="text-sm pl-1">Pada tanggal {tanggalNow}</p>
               <div className="lg:flex lg:space-y-0 lg:space-x-6 space-y-10">
                 <div className="flex-col w-1/2">
@@ -170,9 +170,20 @@ function App() {
               </div>
             </div>
           </div>
-          <button className="btn lg:w-2/3 w-4/5 bg-[#6f1d1b] text-white border-none hover:bg-slate-400 hover:text-slate-800 hover:outline-3">
-            Kirim
-          </button>
+          <a href="#my-modal-2" className="btn lg:w-2/3 w-4/5 bg-[#6f1d1b] text-white border-none hover:bg-slate-400 hover:text-slate-800 hover:outline-3">submit</a>
+          <div className="modal" id="my-modal-2">
+            <div className="modal-box">
+              <a href="#header" className="btn btn-sm btn-circle absolute right-2 top-2">✕</a>
+              <h3 className="font-bold text-lg">Konfirmasi Pengiriman Data</h3>
+              <p className="py-4">Data Sudah DI isi Dengan Kondisi Sadar dan jujur</p>
+              <div className="modal-action">
+              <button className="btn lg:w-2/3 w-4/5 bg-[#6f1d1b] text-white border-none hover:bg-slate-400 hover:text-slate-800 hover:outline-3">
+                Kirim
+              </button>
+
+              </div>
+            </div>
+          </div>
         </div>
       </form>
     </div>
