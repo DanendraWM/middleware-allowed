b0b08fc29b2c414e4d054f6256c69d22e9c6e8ff
Suntet <sanity2828@gmail.com>
2023-01-26 12:04:38 +0700

    nambah tomvol dan foto

--- src/App.js
+++ src/App.js
@@ -17,13 +17,18 @@ function App() {
   };
 
   return (
-    <div className=" min-h-screen bg-slate-300 text-black ">
-      <div className=" flex justify-center items-center py-4 ">
-        <div className="bg-white lg:w-1/2 w-5/6 h-full shadow-[0_6px_6px_-1px_gray] rounded-sm flex flex-col py-4 items-center ">
-          <p className="text-xl font-semibold">- Middleware Entry Request -</p>
-          <hr className="w-1/4 py-1" />
+    <div className=" min-h-screen bg-slate-300 text-black  ">
+      <div className="flex flex-row justify-center w-10 h-10 ">
+        <img src={telkom} className="w-10 h-10"></img>
+        <p>ASSSAASASAS</p>
+      </div>
+
+      <div className=" flex justify-center flex-col items-center py-4 sm:py-25 ">
+        <div className="bg-white lg:w-1/2 w-5/6 h-full shadow-[0_6px_6px_-1px_gray] rounded-sm flex flex-col py-4 items-center rounded-sm	 ">
+          <p className="text-lg sm:text-xl font-semibold p-2">- Middleware Entry Request -</p>
+          <hr className="w-1/4 py-0" />
           <div className="card lg:w-1/2">
-            <div className=" card-body">
+            <div className=" card-body p-2">
               <p className="card-title">
                 <span className="text-rose-600">*</span>Nama Pengaju
               </p>
@@ -40,7 +45,7 @@ function App() {
             </div>
           </div>
           <div className="card lg:w-1/2">
-            <div className=" card-body">
+            <div className=" card-body p-2">
               <p className="card-title">
                 <span className="text-rose-600">*</span>Instansi
               </p>
@@ -56,7 +61,7 @@ function App() {
             </div>
           </div>
           <div className="card lg:w-1/2">
-            <div className=" card-body">
+            <div className=" card-body p-2">
               <p className="card-title">
                 <span className="text-rose-600">*</span>Tujuan
               </p>
@@ -72,7 +77,7 @@ function App() {
             </div>
           </div>
           <div className="card lg:w-1/2">
-            <div className=" card-body">
+            <div className=" card-body p-2">
               <p className="card-title">
                 <span className="text-rose-600">*</span>Keterangan
               </p>
@@ -88,7 +93,7 @@ function App() {
             </div>
           </div>
           <div className="card lg:w-1/2">
-            <div className=" card-body">
+            <div className=" card-body p-2 py-4">
               <p className="card-title">
                 <span className="text-rose-600">*</span>Waktu
               </p>
@@ -114,6 +119,31 @@ function App() {
                   required
                   onChange={(e) => setEndT(e.target.value)}
                 />
+              </div>
+            </div>
+          </div>
+          <a
+            href="#my-modal-2"
+            className="btn lg:w-2/3 w-4/5 bg-[#6f1d1b] text-white border-none hover:bg-slate-400 hover:text-slate-800 hover:outline-3 space-y-2"
+          >
+            submit
+          </a>
+          <div className="modal" id="my-modal-2">
+            <div className="modal-box bg-slate-100">
+              <a
+                href="#header"
+                className="btn btn-sm btn-circle absolute right-2 top-2"
+              >
+                ✕
+              </a>
+              <h3 className="font-bold text-lg">Konfirmasi Pengiriman Data</h3>
+              <p className="py-4">
+                Data Sudah Di isi Dengan Kondisi Sadar dan jujur
+              </p>
+              <div className="modal-action">
+                {/* <button className="btn lg:w-2/3 w-4/5 bg-[#6f1d1b] text-white border-none hover:bg-slate-400 hover:text-slate-800 hover:outline-3">
+                  Kirim
+                </button> */}
               </div>
             </div>
           </div>
